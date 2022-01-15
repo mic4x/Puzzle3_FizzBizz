62ea935f6c70564af62b0ee8ab9738c383cb7b24
mic4x <95673380+mic4x@users.noreply.github.com>
2022-01-14 20:50:47 -0500

    Update puzzle3.js

--- puzzle3.js
+++ puzzle3.js
@@ -1,10 +1,10 @@
-function puzzle(number) {
+function puzzle(number) { //declearing the function puzzle 
 
 
-for (i=1; i<= number; i++) { // for i is an variable that is iterated in the loop starting at 1 and stoping at the variable number passed in
+for (i=1; i<= number; i++) { // for i is an variable that is iterated in the loop starting at 1 and stoping at the variable number passed in. Iterates each time it runs through the functoin.
 
   if(i % 3 == 0 && i % 5 == 0) { // if the remainder of the number 0 after being divided by 3 and 5 then output Fizz Buzz
-        console.log('Fizz Buzz')
+        console.log('Fizz Buzz') //Prints Fizz Buzz to the screen if both Parameters are met 
     }
 
   else if(i % 5 == 0){ // if the remainder of the number 0 after being divided by 5 then output Buzz
@@ -15,10 +15,12 @@ for (i=1; i<= number; i++) { // for i is an variable that is iterated in the loo
 }
 
   else{
-    console.log(i)
+    console.log(i) //prints any of the numbers that don't fit the parameter above 
 }
 
 }
 }
 
 puzzle(200) //passes in 200 for the variable number 
+
+//Final output should only print numbers that arean't divisble my 3 or 5 and print Fizz, Buzz or Fizz Buzz for those that are. 
